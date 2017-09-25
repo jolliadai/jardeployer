1a671826fdd99a9470b5ee3e3211dae5c0e322ee
jollia <dai@jollia.cn>
2017-09-25 16:50:44 +0800

    初始化项目

---
+++ main.go
@@ -0,0 +1,14 @@
+/**
+ * @Descript    :
+ * @Author      : Jollia Dai
+ * @Date        : Created in 2017/09/25 16:47
+ * @Modified By :
+ */
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
